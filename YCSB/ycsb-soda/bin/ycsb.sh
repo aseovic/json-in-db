2b3ce30b90538a3d64c648b1c0ef7878c7db039f
Vaso Putica <vaso.putica@oracle.com>
2022-02-13 18:40:43 +0100

    Implement Coherence client

--- YCSB/ycsb-soda/bin/ycsb.sh
+++ YCSB/ycsb-soda/bin/ycsb.sh
@@ -4,7 +4,7 @@ CLASSPATH=core/target/*:core/target/dependency/*:soda/target/*:soda/target/depen
 
 bin/ycsb $1 $2 -cp $CLASSPATH \
          -P ./workloads/$3 \
-         -jvm-args "-Xmx30g -Xms30g -Dsun.net.inetaddr.ttl=0" \
+         -jvm-args "-Xmx30g -Xms30g -Dsun.net.inetaddr.ttl=0 -Dtangosol.coherence.distributed.localstorage=false -Dtangosol.pof.enabled=true -Dtangosol.coherence.ttl=0 -Dtangosol.coherence.localhost=127.0.0.1 -Djava.net.preferIPv4Stack=true" \
          -s -threads $THREADS \
          -p db.url=jdbc:oracle:thin:@${DB_NAME}?TNS_ADMIN=${TNS_ADMIN} \
          -p db.user=$DB_USER \
